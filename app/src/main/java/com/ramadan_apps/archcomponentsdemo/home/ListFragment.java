2a9dbaee0e675a1834fe2464b8d650b7b7a73203
elsamet <eng.mahmoudramadan2012@gmail.com>
2018-02-02 11:16:53 +0200

    add dependencies for our project

---
+++ app/src/main/java/com/ramadan_apps/archcomponentsdemo/home/ListFragment.java
@@ -0,0 +1,10 @@
+package com.ramadan_apps.archcomponentsdemo.home;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Mahmoud Ramadan on 2/2/18.
+ */
+
+public class ListFragment extends Fragment {
+}
--- app/src/main/java/com/ramadan_apps/archcomponentsdemo/home/MainActivity.java
+++ app/src/main/java/com/ramadan_apps/archcomponentsdemo/home/MainActivity.java
@@ -1,7 +1,9 @@
-package com.ramadan_apps.archcomponentsdemo;
+package com.ramadan_apps.archcomponentsdemo.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ramadan_apps.archcomponentsdemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
