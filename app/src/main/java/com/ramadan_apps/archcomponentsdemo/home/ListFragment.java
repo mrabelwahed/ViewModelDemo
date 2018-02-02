cce8078c793d1e6d25ecc75069325f20662bf385
elsamet <eng.mahmoudramadan2012@gmail.com>
2018-02-02 11:30:56 +0200

    project setup

--- app/src/main/java/com/ramadan_apps/archcomponentsdemo/home/ListFragment.java
+++ app/src/main/java/com/ramadan_apps/archcomponentsdemo/home/ListFragment.java
@@ -1,10 +1,57 @@
 package com.ramadan_apps.archcomponentsdemo.home;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.ramadan_apps.archcomponentsdemo.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by Mahmoud Ramadan on 2/2/18.
  */
 
 public class ListFragment extends Fragment {
+
+    private Unbinder unbinder;
+    @BindView(R.id.recyclerview)
+    RecyclerView recyclerView;
+    @BindView(R.id.tv_error)
+    TextView errorView;
+    @BindView(R.id.progreesbar)
+    ProgressBar loadingView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (unbinder != null) {
+            unbinder.unbind();
+            unbinder = null;
+        }
+    }
 }
